c6d4a046a3aaabe8cb6323cc20cb07d9b2f8493d
James Foster <git@jgfoster.net>
2020-12-19 21:57:12 -0800

    Update to 3.9.1 for AST.

--- AST/singleDump.sh
+++ AST/singleDump.sh
@@ -1,16 +1,9 @@
 VERSION=$(python3 -V | awk '{print $2}')
 OUTPUT=$1
 
-if [ ! -f $VERSION ]; then
+if [ ! -d $VERSION ]; then
     echo "Creating folder $VERSION"
 	mkdir $VERSION
 fi
 
-pprintast "../tests/${OUTPUT}.py" >> "${VERSION}/${OUTPUT}Pretty.txt"
-echo "
-import ast
-file=open('../tests/${OUTPUT}.py','r')
-tree=ast.parse(file.read())
-out=ast.dump(tree,annotate_fields=False,include_attributes=True)
-file.close()
-print(out)" | python3 >> "${VERSION}/${OUTPUT}.txt"
+pprintast -a -t "../tests/${OUTPUT}.py" >> "${VERSION}/${OUTPUT}.ast"
